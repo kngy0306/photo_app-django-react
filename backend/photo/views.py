a827885369ce8d06f968a3bcc6d5ac8ab726053b
Kona <kitami.kitano20@gmail.com>
2021-10-09 21:49:59 +0900

    :sparkles: Create Serializers

--- backend/photo/views.py
+++ backend/photo/views.py
@@ -3,65 +3,57 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, viewsets, filters
 
-from .models import PhotoPost, Category
+from .models import PhotoPost
+from .serializers import PhotoPostSerializer
 
 
-class PhotoList(APIView):
-    def get(self, request):
-        try:
-            photos = PhotoPost.objects.order_by('-posted_at')
-            res = [
-                {
-                    'id': photo.id,
-                    'title': photo.title,
-                    'image': photo.image.url,
-                    'category': photo.category.title,
-                }
-                for photo in photos
-            ]
-            return Response(res)
-        except:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class PhotoListViewSet(viewsets.ModelViewSet):
+    queryset = PhotoPost.objects.all()
+    serializer_class = PhotoPostSerializer
 
 
-class PhotoDetail(APIView):
-    def get(self, request, pk):
-        try:
-            try:
-                photo = PhotoPost.objects.get(id=pk)
-            except:
-                return Response("Not Found", status=status.HTTP_404_NOT_FOUND)
-            res = {
-                'id': photo.id,
-                'title': photo.title,
-                'comment': photo.comment,
-                'image': photo.image.url,
-                'category': photo.category.title,
-                'posted_at': photo.posted_at,
-            }
-            return Response(res)
-        except:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# class PhotoList(APIView):
+#     def get(self, request):
+#         try:
+#             photos = PhotoPost.objects.order_by('-posted_at')
+#             res = [
+#                 {
+#                     'id': photo.id,
+#                     'title': photo.title,
+#                     'image': photo.image.url,
+#                     'category': photo.category.title,
+#                 }
+#                 for photo in photos
+#             ]
+#             return Response(res)
+#         except:
+#             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+#     def post(self, request):
+#         try:
+#             serializer = PhotoPostSerializer(data=request.data)
+#             serializer.is_valid(reaise_exception=True)
+#             serializer.save()
+#             return Response(serializer.data, status.HTTP_201_CREATED)
+#         except:
+#             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class PhotoCategory(APIView):
-    def get(self, requesst, category):
-        try:
-            try:
-                category = Category.objects.get(title=category)
-                photos = PhotoPost.objects.filter(category=category)
-            except:
-                return Response("Not Found", status=status.HTTP_404_NOT_FOUND)
-
-            res = [
-                {
-                    'id': photo.id,
-                    'title': photo.title,
-                    'image': photo.image.url,
-                    'category': photo.category.title,
-                }
-                for photo in photos
-            ]
-            return Response(res)
-        except:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# class PhotoDetail(APIView):
+#     def get(self, request, pk):
+#         try:
+#             try:
+#                 photo = PhotoPost.objects.get(id=pk)
+#             except:
+#                 return Response("Not Found", status=status.HTTP_404_NOT_FOUND)
+#             res = {
+#                 'id': photo.id,
+#                 'title': photo.title,
+#                 'comment': photo.comment,
+#                 'image': photo.image.url,
+#                 'category': photo.category.title,
+#                 'posted_at': photo.posted_at,
+#             }
+#             return Response(res)
+#         except:
+#             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- backend/photo_app/urls.py
+++ backend/photo_app/urls.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from photo.views import PhotoListViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r'photo', PhotoListViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('photo/', include('photo.urls')),
+    path('', include(router.urls)),
 ]
 
 urlpatterns += static(
-    settings.IMAGE_URL, # IMAGE_URL = '/images/'
-    document_root=settings.IMAGE_ROOT # IMAGE_ROOTにリダイレクト
+    settings.IMAGE_URL,
+    document_root=settings.IMAGE_ROOT
 )
