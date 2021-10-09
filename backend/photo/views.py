bc35cc96c298e6f590113d96d613893d53ccd797
Kona <kitami.kitano20@gmail.com>
2021-10-09 09:52:43 +0900

    :sparkles: 投稿をカテゴリ別に表示するAPIを実装

--- backend/photo/views.py
+++ backend/photo/views.py
@@ -3,25 +3,26 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, viewsets, filters
 
-from .models import PhotoPost
+from .models import PhotoPost, Category
 
 
 class PhotoList(APIView):
     def get(self, request):
         try:
-            photo_list = PhotoPost.objects.order_by('-posted_at')
-            res_list = [
+            photos = PhotoPost.objects.order_by('-posted_at')
+            res = [
                 {
                     'id': photo.id,
                     'title': photo.title,
                     'image': photo.image.url,
                     'category': photo.category.title,
                 }
-                for photo in photo_list
+                for photo in photos
             ]
-            return Response(res_list)
+            return Response(res)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class PhotoDetail(APIView):
     def get(self, request, pk):
@@ -38,6 +39,29 @@ class PhotoDetail(APIView):
                 'category': photo.category.title,
                 'posted_at': photo.posted_at,
             }
+            return Response(res)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class PhotoCategory(APIView):
+    def get(self, requesst, category):
+        try:
+            try:
+                category = Category.objects.get(title=category)
+                photos = PhotoPost.objects.filter(category=category)
+            except:
+                return Response("Not Found", status=status.HTTP_404_NOT_FOUND)
+
+            res = [
+                {
+                    'id': photo.id,
+                    'title': photo.title,
+                    'image': photo.image.url,
+                    'category': photo.category.title,
+                }
+                for photo in photos
+            ]
             return Response(res)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
